270c7d032113fce253bdf90afcd9f2d9c4e8bc5c
Tetyana <tetyana.lesivtsiv@gmail.com>
2021-07-15 12:52:24 +0300

    changed homework_11

--- homework_11/js/main.js
+++ homework_11/js/main.js
@@ -1,26 +1,28 @@
 let lengthString = prompt("Введіть кількість символів", 1);
 
 while (isNaN(lengthString)) {
-  lengthString = +Math.trunc(
-      prompt("Помилка! Введіть кількість символів", 1));
+    lengthString = +Math.trunc(
+    prompt("Помилка! Введіть кількість символів", 1));
 }
 
 async function getRandomChinese(length) {
-  let i = 0;
-  let chineseString = "";
-  while (i < length){
+    let i = 0;
+    let chineseString = "";
+
+    while (i < length) {
     const randomChinese = new Promise((resolve, reject) => {
-      setTimeout(() => {
-        const sign = (Date.now().toString().slice(-5));
-        const string = (String.fromCharCode(sign));
+        setTimeout(() => {
+            const sign = (Date.now().toString().slice(-5));
+            const string = (String.fromCharCode(sign));
         resolve (string);
-        reject(string);
+        reject (string);
         i++;
         }, 50);        
     });
     chineseString = chineseString + await randomChinese;   
-  }
-  return chineseString;
+    }
+    
+    return chineseString;
 }
 
 getRandomChinese(lengthString).then(console.log);
