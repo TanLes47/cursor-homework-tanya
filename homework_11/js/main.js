3aa28a7baf49713d534514ce53ecc8e3f584e3e3
Tetyana <tetyana.lesivtsiv@gmail.com>
2021-07-15 12:43:12 +0300

    homework-11

---
+++ homework_11/js/main.js
@@ -0,0 +1,31 @@
+let lengthString = prompt("Введіть кількість символів", 1);
+
+while (isNaN(lengthString)) {
+  lengthString = +Math.trunc(
+      prompt("Помилка! Введіть кількість символів", 1));
+}
+
+async function getRandomChinese(length) {
+  let i = 0;
+  let chineseString = "";
+  while (i < length){
+    const randomChinese = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const sign = (Date.now().toString().slice(-5));
+        const string = (String.fromCharCode(sign));
+        resolve (string);
+        reject(string);
+        i++;
+        }, 50);        
+    });
+    chineseString = chineseString + await randomChinese;   
+  }
+  return chineseString;
+}
+
+getRandomChinese(lengthString).then(console.log);
+
+
+
+
+  
