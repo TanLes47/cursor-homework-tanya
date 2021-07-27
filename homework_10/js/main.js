6f7c5b24a1149d7686fa5fa4738075fcb001fc85
Tetyana <tetyana.lesivtsiv@gmail.com>
2021-07-27 22:33:56 +0300

    changed code

--- homework_10/js/main.js
+++ homework_10/js/main.js
@@ -1,4 +1,4 @@
-/*const keys = document.querySelectorAll('.key');
+const keys = document.querySelectorAll('.key');
 const audio = document.querySelectorAll('audio');
 
 function startPlaying(e) {
@@ -41,47 +41,7 @@ function playOnClick() {
     }
 }
 
-keys.forEach(key => key.addEventListener("click", playOnClick));*/
-
-
-
-
-const keys = ["KeyA", "KeyS", "KeyD", "KeyF", "KeyG"];
-const clickKeys = document.querySelectorAll(".key");
-const audio = document.querySelectorAll("audio");
-console.log(clickKeys)
-
-
-function clickPlay() {
-    clickKeys.forEach(element => element.onclick = function () {
-    audio.forEach(sound => {
-      if (sound.id === element.id) {
-        sound.play();
-        
-      }
-    })
-    })
-}
-
-
-function clickSound() {
-  document.addEventListener("keydown", function (event) {
-    keys.forEach(element => {
-      if (element === event.code) {
-        audio.forEach((sound) => {
-          if (`Key${sound.id}` === `${element}`) {
-            sound.play();
-          }
-        });
-      }
-    })
-  })
-
-  clickPlay();
-}
-clickSound();
-
-
+keys.forEach(key => key.addEventListener("click", playOnClick));
 
 
 
